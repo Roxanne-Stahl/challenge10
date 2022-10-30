fa218731037f9229a86881b4d65935a71857dccc
Roxanne Stahl <109992581+rox-stahl@users.noreply.github.com>
2022-10-30 09:01:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('userForm');
+const email = document.getElementById('userEmail');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const emailVal = email.value;
+
+    //email validation
+    if(!validateEmail(emailVal)){
+        form.classList.add('error');
+    }
+    else {
+        form.classList.remove('error');
+    }
+});
+
+function validateEmail(email) {
+    let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regEx.test(String(email).toLowerCase());
+};
